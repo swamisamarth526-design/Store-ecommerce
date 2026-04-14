5925941f86e63197754b0359a2a88e11a404c837
swamisamarth526-design <swamisamarth526@gmail.com>
2026-04-14 14:39:04 +0530

    Fix bugs

--- server/utils/storage.js
+++ server/utils/storage.js
@@ -1,22 +1,34 @@
-const AWS = require('aws-sdk');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 
 const keys = require('../config/keys');
 const isProduction = process.env.NODE_ENV === 'production';
+
+const buildS3Url = (bucket, region, key) => {
+  const encodedKey = encodeURIComponent(key).replace(/%2F/g, '/');
+  return region === 'us-east-1'
+    ? `https://${bucket}.s3.amazonaws.com/${encodedKey}`
+    : `https://${bucket}.s3.${region}.amazonaws.com/${encodedKey}`;
+};
 
 exports.s3Upload = async image => {
   try {
     let imageUrl = '';
     let imageKey = '';
 
-    if (!keys.aws.accessKeyId && !isProduction) {
-      console.warn('Missing aws keys');
+    if (!keys.aws.accessKeyId || !keys.aws.secretAccessKey || !keys.aws.bucketName) {
+      if (!isProduction) {
+        console.warn('Missing AWS configuration: access keys or bucket name are not set.');
+      }
+      return { imageUrl, imageKey };
     }
 
     if (image) {
-      const s3bucket = new AWS.S3({
-        accessKeyId: keys.aws.accessKeyId,
-        secretAccessKey: keys.aws.secretAccessKey,
-        region: keys.aws.region
+      const client = new S3Client({
+        region: keys.aws.region || 'us-east-1',
+        credentials: {
+          accessKeyId: keys.aws.accessKeyId,
+          secretAccessKey: keys.aws.secretAccessKey
+        }
       });
 
       const params = {
@@ -26,10 +38,10 @@ exports.s3Upload = async image => {
         ContentType: image.mimetype
       };
 
-      const s3Upload = await s3bucket.upload(params).promise();
+      await client.send(new PutObjectCommand(params));
 
-      imageUrl = s3Upload.Location;
-      imageKey = s3Upload.key;
+      imageUrl = buildS3Url(keys.aws.bucketName, keys.aws.region || 'us-east-1', image.originalname);
+      imageKey = image.originalname;
     }
 
     return { imageUrl, imageKey };
